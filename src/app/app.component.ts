e0ffde802a47d299d1e576e21c98e5cac9d8413a
William Mian <william_mian@yahoo.com.br>
2021-02-01 17:18:40 -0300

    teste relatorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { LoginService } from './services/login.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -30,7 +31,7 @@ export class AppComponent implements OnInit {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private loginService: LoginService
+    private http: HttpClient
   ) {
     this.initializeApp();
   }
@@ -47,6 +48,34 @@ export class AppComponent implements OnInit {
     if (path !== undefined) {
       this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
+  }
+
+  testeRelatorio() {
+    console.log('Teste relatório');
+    
+    let usuario = JSON.parse(localStorage.getItem('usuario'));
+    let headers = new HttpHeaders({ 'x-auth-token': usuario.hash });
+
+    let url = `${environment.servidor}/#/report/ui/multitec.relatorios.cgs.CGS_Naturezas`
+
+    this.http.get(url, { headers: headers }).toPromise()
+    .then(response => console.log(response))
+    .catch(response => console.log(response));
+  }
+
+  testeRelatorio2() {
+    console.log('Teste relatório 2');
+
+    let usuario = JSON.parse(localStorage.getItem('usuario'));
+    let headers = new HttpHeaders({ 'x-auth-token': usuario.hash });
+
+    let url = `${environment.servidor}/#/report/ui/multitec.relatorios.cgs.CGS_Naturezas`
+
+    window.open(url, '_blank');
+    //url,
+    //target, '_self' '_blank' '_system'
+    //options
+    
   }
 
 }
